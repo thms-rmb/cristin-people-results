56dce2ac2c0bea2a1661b17795ed494c5a5d8797
Thomas Rambø <thms.rmb@gmail.com>
2019-02-24 16:48:56 +0100

    Role, Employment, Contribution Order, etc.

--- net.py
+++ net.py
@@ -1,69 +1,99 @@
-import concurrent.futures
-import requests
-import json
+import sys, csv
 
-def get_people():
-    def getter(url):
-        resp = requests.get(url, timeout=10)
-        if resp.status_code == requests.codes.ok:
-            for person in resp.json():
-                yield person
+import asyncio, aiohttp
+from requests import codes
+from requests.utils import parse_header_links
 
-        if 'next' in resp.links:
-            next_link = resp.links['next']['url']
-            yield from getter(next_link)
+async def get_people(session):
+    async def getter(url):
+        async with session.get(url) as response:
+            if response.status == codes.ok:
+                for person in await response.json():
+                    yield person
 
-    yield from getter('https://api.cristin.no/v2/persons?institution=dmmh')
+                links = parse_header_links(response.headers.get('link', ''))
+                for link in links:
+                    if link['rel'] == 'next':
+                        async for person in getter(link['url']):
+                            yield person
+    async for person in getter('https://api.cristin.no/v2/persons?institution=dmmh'):
+        yield person
 
-def get_person_results(person, since = 2018, before = 2018):
-    def getter(url):
-        resp = requests.get(url, timeout=5)
-        if resp.status_code == requests.codes.ok:
-            for result in resp.json():
-                yield result
+async def get_person_results(session, person, since = 2018, before = 2018):
+    async def getter(url):
+        async with session.get(url) as response:
+            if response.status == codes.ok:
+                for result in await response.json():
+                    yield result
 
-        if 'next' in resp.links:
-            next_link = resp.links['next']['url']
-            yield from getter(next_link)
+                links = parse_header_links(response.headers.get('link', ''))
+                for link in links:
+                    if link['rel'] == 'next':
+                        async for result in getter(link['url']):
+                            yield result
 
     person_id = person['cristin_person_id']
-    yield from getter("{}?contributor={}&published_since={}&published_before={}".format(
+    async for result in  getter("{}?contributor={}&published_since={}&published_before={}".format(
         'https://api.cristin.no/v2/results', person_id, since, before
-    ))
+    )):
+        yield result
 
-def filter_results(results):
-    ids = set()
-    for result in results:
-        if result['cristin_result_id'] not in ids:
-            ids.add(result['cristin_result_id'])
-            yield result
+async def get_result_contributors(session, result):
+    async with session.get(result['contributors']['url']) as response:
+        if response.status == codes.ok:
+            for contributor in await response.json():
+                for affiliation in contributor['affiliations']:
+                    async with session.get(affiliation['institution']['url']) as response:
+                        if response.status == codes.ok:
+                            institution = await response.json()
+                            affiliation['institution']['institution_name'] = institution['institution_name']
+                    async with session.get(contributor['url']) as response:
+                        if response.status == codes.ok:
+                            person = await response.json()
+                            if 'affiliations' in person:
+                                for employment in person['affiliations']:
+                                    if employment['institution']['cristin_institution_id'] == affiliation['institution']['cristin_institution_id']:
+                                        if employment['active']:
+                                            affiliation['institution']['employment'] = employment
 
-def get_result_contributors(result):
-    resp = requests.get(result['contributors']['url'], timeout=5)
-    if resp.status_code == requests.codes.ok:
-        for contributor in resp.json():
-            yield contributor
+                yield contributor
 
-def enrich_contributor(contributor):
-    for affiliation in contributor['affiliations']:
-        resp = requests.get(affiliation['institution']['url'], timeout=5)
-        if resp.status_code == requests.codes.ok:
-            institution = resp.json()
-            affiliation['institution']['institution_name'] = institution['institution_name']
-    return contributor
+async def main():
+    results = set()
+    out = csv.writer(sys.stdout)
+    out.writerow([
+        'Id', 'Title', 'Category',
+        'Surname', 'First Name',
+        'Contribution Order Nr.', 'Role', 'Employment', 'Institution'
+    ])
+    
+    async with aiohttp.ClientSession() as session:
+        async for person in get_people(session):
+            async for result in get_person_results(session, person):
+                if result['cristin_result_id'] in results:
+                    continue
+
+                results.add(result['cristin_result_id'])
+                _, title = result['title'].popitem()
+                _, category = result['category']['name'].popitem()
+                async for contributor in get_result_contributors(session, result):
+                    for affiliation in contributor['affiliations']:
+                        _, role = affiliation['role']['name'].popitem()
+                        institution = affiliation['institution']['cristin_institution_id']
+                        if 'institution_name' in affiliation['institution']:
+                            _, institution = affiliation['institution']['institution_name'].popitem()
+
+                        employment = 'Unknown'
+                        if 'employment' in affiliation['institution']:
+                            if 'position' in affiliation['institution']['employment']:
+                                _, employment = affiliation['institution']['employment']['position'].popitem()
+                        row = [
+                            result['cristin_result_id'], title, category,
+                            contributor['surname'], contributor['first_name'],
+                            contributor['order'], role, employment, institution
+                        ]
+
+                        out.writerow(row)
 
 if __name__ == '__main__':
-    with concurrent.futures.ThreadPoolExecutor(max_workers=50) as pool:
-        people = get_people()
-        results = filter_results([result for results in pool.map(get_person_results, people) for result in results])
-        contributors = map(get_result_contributors, results)
-        
-        for result, contributors in zip(results, contributors):
-            for contributor in pool.map(enrich_contributor, contributors):
-                for affiliation in contributor['affiliations']:
-                    _, role = affiliation['role']['name'].popitem()
-                    institution = affiliation['institution']['cristin_institution_id']
-                    if 'institution_name' in affiliation['institution']:
-                        _, institution = affiliation['institution']['institution_name'].popitem()
-
-                    print("{},{},{},{}".format(result['cristin_result_id'], contributor['surname'], role, institution))
+    asyncio.run(main())
